e7957982d440bb8956a6f8270a93b03624083994
Super Oleg <oleg_in@bk.ru>
2020-02-14 01:07:55 +0300

    MCC and Currency added, toStrings in Main added

--- src/BankStatementOperations.java
+++ src/BankStatementOperations.java
@@ -1,51 +1,58 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class BankStatementOperations {
 
-    LocalDate date;
-    String name;
-    double income;
-    double expense;
+    private LocalDate date;
+    private String name;
+    private double income;
+    private double expense;
+    private String mcc;
+    private boolean isUSD;
+    private boolean isRUR;
+    private boolean isEUR;
 
     public BankStatementOperations(String date, String name, double income, double expense) {
         this.date = LocalDate.parse(date, DateTimeFormatter.ofPattern("dd.MM.yy"));
         this.name = name;
         this.income = income;
         this.expense = expense;
+        mcc = setMCC(name);
+        setCurrency(name);
     }
 
     public LocalDate getDate() {
         return date;
     }
 
-    public void setDate(String date) {
-        this.date = LocalDate.parse(date, DateTimeFormatter.ofPattern(date));
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public double getIncome() {
         return income;
     }
 
-    public void setIncome(double income) {
-        this.income = income;
-    }
-
     public double getExpense() {
         return expense;
     }
 
-    public void setExpense(double expense) {
-        this.expense = expense;
+    public String getMcc() {
+        return mcc;
+    }
+
+    public boolean isUSD() {
+        return isUSD;
+    }
+
+    public boolean isRUR() {
+        return isRUR;
+    }
+
+    public boolean isEUR() {
+        return isEUR;
     }
 
     @Override
@@ -53,6 +60,27 @@ public class BankStatementOperations {
         return "Дата операции: " + date.format(DateTimeFormatter.ofPattern("dd.MM.yy")) +
                 ", Наименование операции: " + name +
                 ", Приход: " + income +
-                ", Расход: " + expense;
+                ", Расход: " + expense +
+                ", MCC: " + mcc;
+    }
+
+    private String setMCC(String operationName) {
+        Pattern pattern = Pattern.compile("MCC\\d{4}");
+        Matcher matcher = pattern.matcher(operationName);
+        return matcher.find() ? matcher.group() : "Номер MCC не указан в выписке";
+    }
+
+    private void setCurrency(String operationName) {
+        Pattern usd = Pattern.compile("USD{1}");
+        Pattern rur = Pattern.compile("RUR{1}");
+        Pattern eur = Pattern.compile("EUR{1}");
+        Matcher matchUSD = usd.matcher(operationName);
+        Matcher matchRUR = rur.matcher(operationName);
+        Matcher matchEUR = eur.matcher(operationName);
+        if (matchRUR.find()) {
+            isRUR = true;
+        } else if (matchEUR.find()) {
+            isEUR = true;
+        } else if (matchUSD.find()) isUSD = true;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,105 @@
-import com.opencsv.exceptions.CsvValidationException;
+import com.opencsv.exceptions.CsvException;
 
 import java.io.IOException;
+import java.util.TreeMap;
 
 public class Main {
-    public static void main(String[] args) throws IOException, CsvValidationException {
 
-        Parser.parseCSV("data/movementList.csv")
-                .forEach(System.out::println);
+    static final String MOVEMENT_LIST_CSV = "data/movementList.csv";
+    static final String MCC_CSV = "data/MCC.csv";
 
+    static TreeMap<String, Double> expRUR = new TreeMap<>();
+    static TreeMap<String, Double> expUSD = new TreeMap<>();
+    static TreeMap<String, Double> expEUR = new TreeMap<>();
+
+    public static void main(String[] args) throws IOException, CsvException {
+
+        BankStatementParser.parseCSV(MOVEMENT_LIST_CSV, MCC_CSV);
+
+        System.out.println(getHeaderToString());
+
+        System.out.println(getAllMovementsToString());
+
+        getExpenseByMCCToString();
 
     }
+
+    private static String getHeaderToString() {
+        return String.format("\nАналитическая справка по операциям на основе банковской выписки с " +
+                        "%1$td %1$th %1$tY по %2$td %2$th %2$tY",
+                BankStatementParser.getFirstDateINStatement(BankStatementParser.getBsoList()),
+                BankStatementParser.getLastDateInStatement(BankStatementParser.getBsoList()));
+    }
+
+    private static String getAllMovementsToString() {
+        return String.format("\n%7$-75s: %.2f руб."
+                        + "; %.2f EUR" + "; %.2f USD" +
+                        "\n%8$-75s: %.2f руб." +
+                        "; %.2f EUR" + "; %.2f USD",
+                BankStatementParser.getExpenseSumRUR(BankStatementParser.getBsoList()),
+                BankStatementParser.getExpenseSumEUR(BankStatementParser.getBsoList()),
+                BankStatementParser.getExpenseSumUSD(BankStatementParser.getBsoList()),
+                BankStatementParser.getIncomeSumRUR(BankStatementParser.getBsoList()),
+                BankStatementParser.getIncomeSumEUR(BankStatementParser.getBsoList()),
+                BankStatementParser.getIncomeSumUSD(BankStatementParser.getBsoList()),
+                "Общая сумма расходов составила ", "Общая сумма пополнений счета составила: ");
+    }
+
+    private static void getExpenseByMCCToString() {
+
+        getExpenseByMCC();
+
+        System.out.printf("----------------------------------------------------------------------------------------------------%n" +
+                "%60s", "Расходы по категориям:");
+        for (String key : expRUR.keySet()) {
+            System.out.printf("\n%-75s: %.2f руб.", key, expRUR.get(key));
+        }
+        System.out.printf("\n%60s", "В иностранной валюте:");
+        for (String key : expEUR.keySet()) {
+            if (expUSD.containsKey(key)) {
+                System.out.printf("\n%-75s: %.2f EUR %.2f USD", key, expEUR.get(key), expUSD.get(key));
+                expUSD.remove(key);
+            } else {
+                System.out.printf("\n%-75s: %.2f EUR", key, expEUR.get(key));
+            }
+        }
+        for (String key : expUSD.keySet()) {
+            System.out.printf("\n%-75s: %.2f USD", key, expUSD.get(key));
+        }
+    }
+
+    private static void getExpenseByMCC() {
+
+        for (BankStatementOperations bso : BankStatementParser.getBsoList()) {
+            String bsoMCC = bso.getMcc();
+            String keyInExp = BankStatementParser.getMccMap().get(bsoMCC);
+            double expense = bso.getExpense();
+            if (expense > 0 && bso.isRUR()) {
+                if (expRUR.containsKey(keyInExp)) {
+                    double expenseSum = expRUR.get(keyInExp) + expense;
+                    expRUR.put(keyInExp, expenseSum);
+                } else {
+                    expRUR.put(keyInExp, expense);
+                }
+            } else if (expense > 0 && bso.isUSD()) {
+                if (expUSD.containsKey(keyInExp)) {
+                    double expenseSum = expUSD.get(keyInExp) + expense;
+                    expUSD.put(keyInExp, expenseSum);
+                } else {
+                    expUSD.put(keyInExp, expense);
+                }
+            } else if (expense > 0 && bso.isEUR()) {
+                if (expEUR.containsKey(keyInExp)) {
+                    double expenseSum = expEUR.get(keyInExp) + expense;
+                    expEUR.put(keyInExp, expenseSum);
+                } else {
+                    expEUR.put(keyInExp, expense);
+                }
+
+            }
+        }
+    }
 }
+
+
+
